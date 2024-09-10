3fd973ddb1806da9eff5cc0b226044b2060f42bb
Ederson Moreira <eder.dev1@gmail.com>
2024-09-10 15:03:40 -0300

    10/09/2024

--- screen_match.php
+++ screen_match.php
@@ -4,9 +4,19 @@ echo "Bem_vindo ao ScreenMatch!!
 ";
 
 $nomeFilme = "Top Gun - Maverick";
-$anoDeLancamento = 2022;
+$anoDeLancamento = $argv[1] ?? 2022;
 $nota = 8.8;
 $incluidoNoPlano = true;
 $nota2 = 9.0;
 
-echo $nomeFilme;
+echo "Nome do Filme: $nomeFilme\n" ;
+echo "Nota filme: " . $nota . "\n"; 
+echo "Ano de lançamento do filme: $anoDeLancamento\n" ;
+
+if($anoDeLancamento > 2022){   
+    echo "Esse filme é um lançamento";
+}elseif($anoDeLancamento > 2020 && $anoDeLancamento <= 2022){
+    echo "Esse filme é novo";
+}else{
+    echo "Esse não é um filme novo";
+}
