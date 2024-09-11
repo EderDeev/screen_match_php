dfe3a50ec3db02a55d55aeb2e42a6466f8fca122
Ederson Moreira <eder.dev1@gmail.com>
2024-09-11 12:56:31 -0300

    Finalizando Controlando fluxo

--- screen_match.php
+++ screen_match.php
@@ -4,19 +4,36 @@ echo "Bem_vindo ao ScreenMatch!!
 ";
 
 $nomeFilme = "Top Gun - Maverick";
+
 $anoDeLancamento = $argv[1] ?? 2022;
-$nota = 8.8;
+$somaNotas = 0;
+$quantidadeNotas = $argc - 1; 
+
 $incluidoNoPlano = true;
-$nota2 = 9.0;
+
+for ($i = 1 ; $i < $argc;$i++ ){
+    $somaNotas += $argv[$i];
+}
+$notaFilme = $somaNotas / $quantidadeNotas;
+
 
 echo "Nome do Filme: $nomeFilme\n" ;
-echo "Nota filme: " . $nota . "\n"; 
+echo "Nota filme: " . $notaFilme . "\n"; 
 echo "Ano de lançamento do filme: $anoDeLancamento\n" ;
 
 if($anoDeLancamento > 2022){   
-    echo "Esse filme é um lançamento";
+    echo "Esse filme é um lançamento\n";
 }elseif($anoDeLancamento > 2020 && $anoDeLancamento <= 2022){
-    echo "Esse filme é novo";
+    echo "Esse filme é novo\n";
 }else{
-    echo "Esse não é um filme novo";
+    echo "Esse não é um filme novo\n";
 }
+
+$genero = match($nomeFilme){
+    "Top Gun - Maverick" => "ação",
+    "Thor" => "super-heroi",
+    "Se beber não case" => "comédia",
+    default => "Gênero desconhecido",
+};
+
+echo "Gênero do filme é: $genero";
